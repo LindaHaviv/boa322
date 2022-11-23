83b74412e6d976fe7f47dc410745a2eb73e27146
Haviv <lhaviv@3c06303e1816.ant.amazon.com>
2022-11-23 17:57:09 -0500

    update headers ml inference lambda

--- ml-inference/app.py
+++ ml-inference/app.py
@@ -61,8 +61,8 @@ def lambda_handler(event, context):
         'statusCode': 200,
          'headers': {
             'Access-Control-Allow-Headers': 'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token',
-            "Access-Control-Allow-Methods": 'OPTIONS,POST',
-            "Access-Control-Allow-Origin": "*"
+            'Access-Control-Allow-Methods': 'OPTIONS,POST',
+            'Access-Control-Allow-Origin': '*'
         },
         'body': json.dumps(
             {
